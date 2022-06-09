6c6e82851e6c0fda86cae174f01e82f5c8d89a5c
yudhath <32191663+yudhath@users.noreply.github.com>
2022-06-09 18:51:40 +0700

    Add files via upload

--- featureToggle/featureToggle.go
+++ featureToggle/featureToggle.go
@@ -7,7 +7,6 @@ import (
 	"errors"
 	"io/ioutil"
 	"math/rand"
-	"os"
 	"strings"
 	"time"
 
@@ -29,7 +28,7 @@ func SetAppEnv(value string) {
 }
 
 func IsEnabled(s3Client *s3.Client, featureName string) bool {
-	err := checkBucketName()
+	err := validateMandatoryVariable()
 	if err != nil {
 		log.Info("Error when invoke IsEnabled() method for featureName ", featureName, " due to ", err)
 		return false
@@ -47,7 +46,7 @@ func IsEnabled(s3Client *s3.Client, featureName string) bool {
 func UpsertFeatureToggleConfig(s3Client *s3.Client, featureToggleConfig *FeatureToggleConfig) (bool, error) {
 	prefix := strings.ToLower(appEnv) + "/"
 
-	err := checkBucketName()
+	err := validateMandatoryVariable()
 	if err != nil {
 		return false, err
 	}
@@ -78,12 +77,38 @@ func UpsertFeatureToggleConfig(s3Client *s3.Client, featureToggleConfig *Feature
 
 	resp, err := s3Client.PutObject(context.TODO(), input)
 	if err != nil {
-		log.Error("Failed when upload feature toggle config due to ", err)
-		return false, nil
+		log.Error("Failed when upsert feature toggle config due to ", err)
+		return false, err
 	}
 
 	log.Info("Upsert feature toggle config was success with response from AWS : ", resp)
 	return true, err
+}
+
+func DeleteFeatureToggleConfig(s3Client *s3.Client, featureName string) (bool, error) {
+	prefix := strings.ToLower(appEnv) + "/"
+
+	err := validateMandatoryVariable()
+	if err != nil {
+		return false, err
+	}
+
+	if featureName == "" {
+		return false, errors.New("featureName must not empty")
+	}
+
+	input := &s3.DeleteObjectInput{
+		Bucket: aws.String(bucketName),
+		Key: aws.String(prefix + featureName + ".json"),
+	}
+
+	resp, err := s3Client.DeleteObject(context.TODO(), input)
+	if err != nil {
+		log.Error("Failed when delete feature toggle config due to ", err)
+		return false, err
+	}
+	log.Info("Delete feature toggle config was success with response from AWS : ", resp)
+	return true, nil
 }
 
 func getFeatureToggleConfig(s3Client *s3.Client, featureName string) (*FeatureToggleConfig, error) {
@@ -127,24 +152,13 @@ func isEnabledPartially(percentage int) bool {
 	return randomizer.Intn(100) < percentage
 }
 
-func GetJsonFromS3(s3Client *s3.Client, T interface{}, key string) error {
-	env := os.Getenv("APP_ENV")
-	prefix := strings.ToLower(env) + "/"
-	requestInput := &s3.GetObjectInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String(prefix + key),
-	}
-
-	resp, err := s3Client.GetObject(context.TODO(), requestInput)
-	if err != nil {
-		return err
-	}
-	return json.NewDecoder(resp.Body).Decode(T)
-}
-
-func checkBucketName() error {
+func validateMandatoryVariable() error {
 	if bucketName == "" {
-		return errors.New("bucket name was empty, you must set s3 bucketname using SetBucketName() method before you use any method from this package")
+		return errors.New("bucket name was empty, you must set s3 bucketname using SetBucketName() method before you use any method featuretoggle package")
+	}
+
+	if appEnv == "" {
+		return errors.New("application environment was empty, you must set application envinronment using SetAppEnv() method before you use any method from featuretoggle package")
 	}
 
 	return nil
