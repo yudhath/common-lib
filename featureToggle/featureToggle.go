a0f2ec3a0155caa00959adcd1328b5d263fbb4b1
yudhath <32191663+yudhath@users.noreply.github.com>
2022-06-09 18:13:38 +0700

    Add files via upload

--- featureToggle/featureToggle.go
+++ featureToggle/featureToggle.go
@@ -24,7 +24,64 @@ func SetBucketName(name string) {
 	bucketName = name
 }
 
-type FeatureToggleService struct {}
+func IsEnabled(s3Client *s3.Client, featureName string) (bool, error) {
+	err := checkBucketName()
+	if err != nil {
+		return false, err
+	}
+
+	featureToggleConfig, err := getFeatureToggleConfig(s3Client, featureName)
+	if err != nil {
+		log.Error("Failed when get feature toggle due to ", err)
+		return false, err
+	}
+	
+	isEnabled := featureToggleConfig != (&FeatureToggleConfig{}) && featureToggleConfig.IsEnabled && isEnabledPartially(featureToggleConfig.Percentage)
+	return isEnabled, nil
+}
+
+func UpsertFeatureToggleConfig(s3Client *s3.Client, featureToggleConfig *FeatureToggleConfig) (bool, error) {
+	env := os.Getenv("APP_ENV")
+	prefix := strings.ToLower(env) + "/"
+
+	err := checkBucketName()
+	if err != nil {
+		return false, err
+	}
+
+	if featureToggleConfig != nil || featureToggleConfig == (&FeatureToggleConfig{}) {
+		return false, errors.New("feature toggle struct must not empty/nil")
+	}
+
+	if featureToggleConfig.Name == "" {
+		return false, errors.New("feature toggle name must not empty")
+	}
+
+	if featureToggleConfig.Percentage < 0 || featureToggleConfig.Percentage > 100 {
+		return false, errors.New("feature toggle percentage value must greater than 0 and less than 100")
+	}
+
+	json, err := json.Marshal(featureToggleConfig)
+	if err != nil {
+		log.Error("Failed when try to marshal config due to ", err)
+		return false, err
+	}
+
+	input := &s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(prefix + featureToggleConfig.Name + ".json"),
+		Body:   manager.ReadSeekCloser(bytes.NewReader(json)),
+	}
+
+	resp, err := s3Client.PutObject(context.TODO(), input)
+	if err != nil {
+		log.Error("Failed when upload json due to ", err)
+		return false, nil
+	}
+
+	log.Info("Upsert feature toggle was success with response from AWS : ", resp)
+	return true, err
+}
 
 func getFeatureToggleConfig(s3Client *s3.Client, featureName string) (*FeatureToggleConfig, error) {
 	requestInput := &s3.GetObjectInput{
@@ -61,53 +118,6 @@ func getFeatureToggleConfig(s3Client *s3.Client, featureName string) (*FeatureTo
 	return &featureToggleConfig, err
 }
 
-func (fts *FeatureToggleService) UpsertFeatureToggleConfig(s3Client *s3.Client, featureToggleConfig *FeatureToggleConfig) (bool, error) {
-	env := os.Getenv("APP_ENV")
-	prefix := strings.ToLower(env) + "/"
-
-	if featureToggleConfig != nil || featureToggleConfig == (&FeatureToggleConfig{}) {
-		return false, errors.New("feature toggle struct must not empty/nil")
-	}
-
-	if featureToggleConfig.Name == "" {
-		return false, errors.New("feature toggle name must not empty")
-	}
-
-	if featureToggleConfig.Percentage < 0 || featureToggleConfig.Percentage > 100 {
-		return false, errors.New("feature toggle percentage value must greater than 0 and less than 100")
-	}
-
-	json, err := json.Marshal(featureToggleConfig)
-	if err != nil {
-		log.Error("Failed when try to marshal config due to ", err)
-		return false, err
-	}
-
-	input := &s3.PutObjectInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String(prefix + featureToggleConfig.Name + ".json"),
-		Body:   manager.ReadSeekCloser(bytes.NewReader(json)),
-	}
-
-	resp, err := s3Client.PutObject(context.TODO(), input)
-	if err != nil {
-		log.Error("Failed when upload json due to ", err)
-		return false, nil
-	}
-
-	log.Info("Upsert feature toggle was success with response from AWS : ", resp)
-	return true, err
-}
-
-func (fts *FeatureToggleService) IsEnabled(s3Client *s3.Client, featureName string) bool {
-	featureToggleConfig, err := getFeatureToggleConfig(s3Client, featureName)
-	if err != nil {
-		log.Error("Failed when get feature toggle due to ", err)
-		return false
-	}
-	return featureToggleConfig != (&FeatureToggleConfig{}) && featureToggleConfig.IsEnabled && isEnabledPartially(featureToggleConfig.Percentage)
-}
-
 func isEnabledPartially(percentage int) bool {
 	source := rand.NewSource(time.Now().UnixNano())
 	randomizer := rand.New(source)
@@ -128,4 +138,12 @@ func GetJsonFromS3(s3Client *s3.Client, T interface{}, key string) error {
 		return err
 	}
 	return json.NewDecoder(resp.Body).Decode(T)
+}
+
+func checkBucketName() error {
+	if bucketName == "" {
+		return errors.New("bucket name was empty, you must set s3 bucketname using SetBucketName() method before you any method from this package")
+	}
+
+	return nil
 }
